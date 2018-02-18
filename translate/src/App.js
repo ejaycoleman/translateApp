829c0178850295d8092b9161c570e330da8a2b79
ejaycoleman <ejaycoleman@googlemail.com>
2018-02-18 14:50:26 +0000

    started basic UI (AntD)

--- translate/src/App.js
+++ translate/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+
+
+
 import './App.css';
+import { Layout, Menu, Icon } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 var timer;
 
 class Tooltip extends Component {
+
+
+
   render() {
+    var translatedStr = "Esta es un asombroso oración"
+    var translatedArr = translatedStr.split(" ");
     return (
       <span>
-        yes
+        {translatedArr[this.props.word]}
       </span>
     )
   }
@@ -42,9 +53,10 @@ class Word extends Component {
 
   render() {
     return (
-      <span uid={this.props.uid} onMouseOver={() => this.handleHoverOn(this.props.uid)} onMouseLeave={() => this.handleHoverOff()}>
+      <span uid={this.props.uid} onMouseOver={() => this.handleHoverOn(this.props.uid)} onMouseLeave={() => this.handleHoverOff()} >
+
         {this.props.contents}
-        {this.state.currentWord == this.props.uid? <Tooltip />: ""}
+        {this.state.currentWord === this.props.uid? <Tooltip word={this.state.currentWord}/>: ""}
       </span>
     )
   }
@@ -52,7 +64,7 @@ class Word extends Component {
 
 class App extends Component {
   render() {
-    var str = "This is an amazing sentence.";
+    var str = "This is an amazing sentence";
     var words = str.split(" ");
 
     var rowsArr = [];
@@ -62,9 +74,31 @@ class App extends Component {
 				);
 		}
 
+
     return (
       <div className="App">
-        {rowsArr}
+      <Layout>
+          <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0}}>
+            <h1 style={{color: 'white'}}>
+              Hover over a word
+            </h1>
+          </Sider>
+          <Layout style={{ marginLeft: 200 }}>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <h1>
+                TranslatR
+              </h1>
+            </Header>
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+              <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                {rowsArr}
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Ant Design ©2016 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
       </div>
     );
   }

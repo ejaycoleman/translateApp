a9332e0aca77a59f6ae5f535a5d0f8a667e353ad
ejaycoleman <ejaycoleman@googlemail.com>
2018-02-19 17:18:20 +0000

    using an API exmaple to get an idea around MongoDB

---
+++ server/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : 'mongodb://node:node@ds141028.mlab.com:41028/translateapp'
+};
--- server/server.js
+++ server/server.js
@@ -1,38 +1,35 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
 
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://devusr:dbPass123@ds141028.mlab.com:41028/translateapp');
-
-// Handle the connection event
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function() {
-  console.log("DB connection alive");
-});
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const app            = express();
 
 
+
+
+const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
 
-var router = express.Router();
 
-router.get('/', function(req, res) {
-  var translateString = generateTranslateRequest("this", "yes this is an API");
-    res.json({ string: translateString });
+//mongoose.connect('mongodb://node:node@ds141028.mlab.com:41028/translateapp');
 
 
-});
-
-app.use('/api', router);
-
-app.listen(port);
-console.log('listening on localhost:' + port);
+// router.get('/', function(req, res) {
+//   var translateString = generateTranslateRequest("this", "yes this is an API");
+//     res.json({ string: translateString });
+//
+//
+// });
 
 
 
